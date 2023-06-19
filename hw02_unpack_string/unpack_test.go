e1f5a3b8fd6a64fe303219a34efd9ff1d68dd90f
Serge S. Yuriev <me@nevian.org>
2023-06-19 22:37:53 +0300

    hw02 done

--- hw02_unpack_string/unpack_test.go
+++ hw02_unpack_string/unpack_test.go
@@ -17,10 +17,12 @@ func TestUnpack(t *testing.T) {
 		{input: "", expected: ""},
 		{input: "aaa0b", expected: "aab"},
 		// uncomment if task with asterisk completed
-		// {input: `qwe\4\5`, expected: `qwe45`},
-		// {input: `qwe\45`, expected: `qwe44444`},
-		// {input: `qwe\\5`, expected: `qwe\\\\\`},
-		// {input: `qwe\\\3`, expected: `qwe\3`},
+		{input: `qwe\4\5`, expected: `qwe45`},
+		{input: `qwe\45`, expected: `qwe44444`},
+		{input: `qwe\\5`, expected: `qwe\\\\\`},
+		{input: `qwe\\\3`, expected: `qwe\3`},
+		// new case: repeat escaped digit after escaped \
+		{input: `qwe\\\33`, expected: `qwe\333`},
 	}
 
 	for _, tc := range tests {
@@ -34,7 +36,8 @@ func TestUnpack(t *testing.T) {
 }
 
 func TestUnpackInvalidString(t *testing.T) {
-	invalidStrings := []string{"3abc", "45", "aaa10b"}
+	//new cases: to many digits after escaping, unfinished escaping
+	invalidStrings := []string{"3abc", "45", "aaa10b", `qwe\\\333`, `qwe\\\3\`}
 	for _, tc := range invalidStrings {
 		tc := tc
 		t.Run(tc, func(t *testing.T) {
