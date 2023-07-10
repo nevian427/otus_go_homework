ceed0c32744031bc0bcd97cc8f16851f1bd3ba1a
Serge S. Yuriev <me@nevian.org>
2023-07-10 16:13:30 +0300

    hw03 done

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,6 +1,41 @@
 package hw03frequencyanalysis
 
-func Top10(_ string) []string {
-	// Place your code here.
-	return nil
+import (
+	"regexp"
+	"sort"
+	"strings"
+)
+
+var (
+	clr_spec_chr = regexp.MustCompile(`[!?;:,.'"*+<>=#%&/(){}~|\[\]\\]`)
+	clr_dash     = regexp.MustCompile(`\s-\s`)
+)
+
+func Top10(t string) []string {
+	t = clr_dash.ReplaceAllLiteralString(clr_spec_chr.ReplaceAllLiteralString(strings.ToLower(t), " "), "")
+
+	freq := make(map[string]int)
+	words := strings.Fields(t)
+
+	for w := range words {
+		freq[words[w]]++
+	}
+
+	freqInverse := make(map[int][]string)
+
+	for k, v := range freq {
+		freqInverse[v] = append(freqInverse[v], k)
+	}
+
+	res := make([]string, 0, 10)
+	for f := len(t) - 1; f > 0; f-- {
+		sort.Strings(freqInverse[f])
+		for w := range freqInverse[f] {
+			if len(res) == 10 {
+				break
+			}
+			res = append(res, freqInverse[f][w])
+		}
+	}
+	return res
 }
